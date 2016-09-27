1bfaf2f7c2452643f31c4b9ed1a28ab64203fb00
djrasmus <djrasmus@oakland.edu>
2016-09-27 00:23:57 -0400

    Add files via upload
    fixed again

--- Questionnaire.java
+++ Questionnaire.java
@@ -18,26 +18,29 @@ public class Questionnaire extends Activity implements View.OnClickListener{
     float rat[];
     Button doneButton;
     Button submitButton;
-    RatingBar ratingBar4 = (RatingBar)findViewById(R.id.ratingBar4);
-    RatingBar ratingBar5 = (RatingBar)findViewById(R.id.ratingBar5);
-    RatingBar ratingBar6 = (RatingBar)findViewById(R.id.ratingBar6);
-    RatingBar ratingBar7 = (RatingBar)findViewById(R.id.ratingBar7);
-    RatingBar ratingBar8 = (RatingBar)findViewById(R.id.ratingBar8);
-    RatingBar ratingBar9 = (RatingBar)findViewById(R.id.ratingBar9);
-    RatingBar ratingBar10 = (RatingBar)findViewById(R.id.ratingBar10);
-    RatingBar ratingBar11 = (RatingBar)findViewById(R.id.ratingBar11);
-    RatingBar ratingBar12 = (RatingBar)findViewById(R.id.ratingBar12);
-    RatingBar ratingBar13 = (RatingBar)findViewById(R.id.ratingBar13);
-    RatingBar ratingBar14 = (RatingBar)findViewById(R.id.ratingBar14);
-    RatingBar ratingBar15 = (RatingBar)findViewById(R.id.ratingBar15);
-    RatingBar ratingBar16 = (RatingBar)findViewById(R.id.ratingBar16);
-    RatingBar ratingBar17 = (RatingBar)findViewById(R.id.ratingBar17);
-    RatingBar ratingBar18 = (RatingBar)findViewById(R.id.ratingBar18);
-    RatingBar ratingBar19 = (RatingBar)findViewById(R.id.ratingBar19);
-    RatingBar ratingBar20 = (RatingBar)findViewById(R.id.ratingBar20);
-    RatingBar ratingBar3 = (RatingBar)findViewById(R.id.ratingBar3);
-    RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar);
-    RatingBar ratingBar2 = (RatingBar)findViewById(R.id.ratingBar2);
+    RatingBar ratingBar;
+    RatingBar ratingBar2;
+    TextView rating;
+    RatingBar ratingBar4;
+    RatingBar ratingBar5;
+    RatingBar ratingBar6;
+    RatingBar ratingBar7;
+    RatingBar ratingBar8;
+    RatingBar ratingBar9;
+    RatingBar ratingBar10;
+    RatingBar ratingBar11;
+    RatingBar ratingBar12;
+    RatingBar ratingBar13;
+    RatingBar ratingBar14;
+    RatingBar ratingBar15;
+    RatingBar ratingBar16;
+    RatingBar ratingBar17;
+    RatingBar ratingBar18;
+    RatingBar ratingBar19;
+    RatingBar ratingBar20;
+    RatingBar ratingBar3;
+
+
 
 
     @Override
@@ -49,10 +52,11 @@ public class Questionnaire extends Activity implements View.OnClickListener{
         doneButton.setOnClickListener(this);
         submitButton = (Button) findViewById(R.id.SubmitButton);
         submitButton.setOnClickListener(this);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+       // getMenuInflater().inflate(R.menu.menu_main, menu);
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
@@ -66,21 +70,44 @@ public class Questionnaire extends Activity implements View.OnClickListener{
         if (id==android.R.id.home) {
             finish();
         }
-        if (id == R.id.History) {
+       /* if (id == R.id.History) {
             startActivity(new Intent(getApplicationContext(), History.class));
             return true;
-        }
+        }*/
         return super.onOptionsItemSelected(item);
     }
-    private void doneButtonClick() {
+   /* private void doneButtonClick() {
 
         Intent myIntent = new Intent(getApplicationContext(), MainMenu.class);
         //myIntent.putExtra("userInput", userInput.getText().toString());
         startActivity(myIntent);
     }
-
+*/
     private void submitButtonClick() {
-        rat = new float[19];
+
+        rat = new float[20];
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingBar2 = (RatingBar)findViewById(R.id.ratingBar2);
+        rating = (TextView)findViewById(R.id.textView24);
+        rating.setText("");
+        ratingBar4 = (RatingBar)findViewById(R.id.ratingBar4);
+        ratingBar5 = (RatingBar)findViewById(R.id.ratingBar5);
+        ratingBar6 = (RatingBar)findViewById(R.id.ratingBar6);
+        ratingBar7 = (RatingBar)findViewById(R.id.ratingBar7);
+        ratingBar8 = (RatingBar)findViewById(R.id.ratingBar8);
+        ratingBar9 = (RatingBar)findViewById(R.id.ratingBar9);
+        ratingBar10 = (RatingBar)findViewById(R.id.ratingBar10);
+        ratingBar11 = (RatingBar)findViewById(R.id.ratingBar11);
+        ratingBar12 = (RatingBar)findViewById(R.id.ratingBar12);
+        ratingBar13 = (RatingBar)findViewById(R.id.ratingBar13);
+        ratingBar14 = (RatingBar)findViewById(R.id.ratingBar14);
+        ratingBar15 = (RatingBar)findViewById(R.id.ratingBar15);
+        ratingBar16 = (RatingBar)findViewById(R.id.ratingBar16);
+        ratingBar17 = (RatingBar)findViewById(R.id.ratingBar17);
+        ratingBar18 = (RatingBar)findViewById(R.id.ratingBar18);
+        ratingBar19 = (RatingBar)findViewById(R.id.ratingBar19);
+        ratingBar20 = (RatingBar)findViewById(R.id.ratingBar20);
+        ratingBar3 = (RatingBar)findViewById(R.id.ratingBar3);
         rat[1] = ratingBar2.getRating();
         rat[2] = ratingBar3.getRating();
         rat[3] = ratingBar4.getRating();
@@ -102,7 +129,8 @@ public class Questionnaire extends Activity implements View.OnClickListener{
         rat[19] = ratingBar20.getRating();
         rat[0] = ratingBar.getRating();
         for(int i = 0; i<rat.length; i++){
-            ((TextView)findViewById(R.id.textView24)).setText(((TextView)findViewById(R.id.textView24)).getText() + String.valueOf(rat[i]));
+           // ((TextView)findViewById(R.id.textView24)).setText(((TextView)findViewById(R.id.textView24)).getText() + String.valueOf(rat[i]));
+            rating.append(String.valueOf(rat[i])+ " ");
         }
 
 
@@ -113,9 +141,9 @@ public class Questionnaire extends Activity implements View.OnClickListener{
 
         switch (v.getId())
         {
-            case R.id.DoneButton:
+           /* case R.id.DoneButton:
                 doneButtonClick();
-                break;
+                break;*/
             case R.id.SubmitButton:
                 submitButtonClick();
                 break;
