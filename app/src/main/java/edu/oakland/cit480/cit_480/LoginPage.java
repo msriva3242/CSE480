73869ed169308fb0914160ccde09371a3e3b9edb
Sean Poynter <sjpoynter@oakland.edu>
2016-10-02 13:46:05 -0400

    Updated 10/02/2016

---
+++ app/src/main/java/edu/oakland/cit480/cit_480/LoginPage.java
@@ -0,0 +1,14 @@
+package edu.oakland.cit480.cit_480;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LoginPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+}
